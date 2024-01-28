b23c94ecb855f2b6710e6f2aaa646af1aa9dce0b
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-01-28 20:28:42 -0300

    Componente Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {ButtonContainer} from "./styles";
+
+const Button = () => {
+    return (
+        <ButtonContainer>Buscar</ButtonContainer>
+    )
+}
+export default Button
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import gitLogo from "../../assets/github.png"
 import {Container} from "./styles";
 import Input from "../../components/Input";
 import ItemRepo from "../../components/ItemRepo";
+import Button from "../../components/Button";
 
 const Index = () => {
+    const [repos, setRepos] =  useState([])
     return (
         <Container className="App">
             <img src={gitLogo} alt="Logo github" width={72} height={72}/>
             <Input/>
+            <Button/>
             <ItemRepo/>
         </Container>
     );
